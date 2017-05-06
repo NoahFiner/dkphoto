e67fc00849ba1daf7f2771b7fce20c4f593243c8
Noah Finer <noahfiner@gmail.com>
2017-05-05 21:02:00 -0600

    Make the website have a gallery, sample areas, and a dope af intro

--- script.js
+++ script.js
@@ -22,6 +22,53 @@ $(document).ready(function(){
     } // End if
   });
 
+  //shuffle array
+  function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+  }
 
-  $("#gallery")
+  //intro won't make sense if javascript is disabled
+  $("#intro-center").removeClass("hidden");
+
+  //intro
+  var introAnimation = function(time) {
+    var descs = ["dependable", "beautiful", "intimate", "affordale", "from friends", "amazing", "incredible", "awesome", "sick", "dope", "lit", "cool", "wonderful", "electrifying", "nice", "priceless", "compassionate", "adventerous", "amiable", "courageous", "competitive", "courteous", "empathetic", "exuberant", "generous", "inventive", "innovative", "persistent", "reliable", "sincere", "witty", "bright", "calm", "careful", "creative", "artistic", "artsy", "dynamic", "faithful", "honest", "imaginative", "pioneering", "practical", "peaceful", "reserved", "sensitive", "sympathetic", "thoughtful", "versatile", "understanding"];
+    descs = descs.concat(descs);
+    shuffle(descs);
+    for(i = 0; i < descs.length; i++) {
+      $("#intro-descs").append("<p class='descs hidden' style='transition: opacity 0.25s, transform 0.25s, -webkit-trasnform 0.25s; transition-delay: "+(i*0.025*Math.random())+"s'>"+descs[i]+"</p>");
+    }
+    setTimeout(function() {$(".descs").removeClass("hidden")}, 100);
+    setTimeout(function() {$(".descs").addClass("hidden")}, 100 + time);
+    setTimeout(function() {$(".descs").remove()}, time + descs.length*0.025*1000);
+  }
+
+  introAnimation(4000);
+  setInterval(function() {introAnimation(4000)}, 7000);
+
+  //gallery
+  var imgsHidden = true;
+  var imgs = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
+  var addImgs = function() {
+    imgsHidden = false;
+    for(i = 0; i < imgs.length; i++) {
+      $("#gallery-outer").append("<img id='"+imgs[i]+"' src='img/"+imgs[i]+"' class='photo' />");
+    }
+  }
+
+  if($(window).scrollTop() > ($("#gallery-outer").offset().top - 1000) && imgsHidden) {
+    addImgs();
+  }
+  $(window).scroll(function() {
+    if($(window).scrollTop() > ($("#gallery-outer").offset().top - 1000) && imgsHidden) {
+      addImgs();
+    }
+  });
+
 });
